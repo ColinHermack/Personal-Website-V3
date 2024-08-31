ee6ecd9ed60c24028dc767d91139a9442ed71a9b
Colin Hermack <chermack@purdue.edu>
2024-08-31 17:26:43 -0400

    Fixed build errors

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,10 @@
-import { getProjects } from 'app/projects/utils';
-
 export const baseUrl = 'https://colinhermack.com'
 
 export default async function sitemap() {
-  let projects = getProjects().map((project) => {
-    url: `${baseUrl}/projects/${project.slug}`
-  })
-
   let routes = ['', '/blog'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...projects]
+  return [...routes]
 }
